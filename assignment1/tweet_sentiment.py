02bd090f5df55808caa1d64d54aa190d8a7c834f
lg9 <lg@u.washington.edu>
2015-12-11 17:18:47 -0800

    Revert "Finished problem 2"
    This reverts commit 33f0219e7e32374801ee779aeabadbca6d1feb20.

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,5 +1,4 @@
 import sys
-import json
 
 def hw():
     print 'Hello, world!'
@@ -7,40 +6,12 @@ def hw():
 def lines(fp):
     print str(len(fp.readlines()))
 
-def get_sent_dict(sentiment_file):
-    scores = {}
-    finn_fh = open(sentiment_file)
-    for line in finn_fh.readlines():
-        term, score = line.split("\t")
-        scores[term] = int(score)
-    finn_fh.close()
-    return scores
-
-def print_tweet_scores(tweet_json_file, sentiment_dictionary):
-    tweet_fh = open(tweet_json_file)
-    for line in tweet_fh:
-        tweet_json = json.loads(line)
-        if 'text' in tweet_json:        # tweets have 'text' field
-            # since it is a tweet, calculate and print sentiment score
-            score = 0
-            words = tweet_json["text"].split(' ')
-            for word in words:
-                if word in sentiment_dictionary:
-                    score += sentiment_dictionary[word]
-            print str(score)
-    tweet_fh.close()
-
 def main():
-    sent_filename = sys.argv[1]
-    sent_dict = get_sent_dict(sent_filename)
-    tweet_filename = sys.argv[2]
-    print_tweet_scores(tweet_filename, sent_dict)
-    
-##    sent_file = open(sys.argv[1])
-##    tweet_file = open(sys.argv[2])
-##    hw()
-##    lines(sent_file)
-##    lines(tweet_file)
+    sent_file = open(sys.argv[1])
+    tweet_file = open(sys.argv[2])
+    hw()
+    lines(sent_file)
+    lines(tweet_file)
 
 if __name__ == '__main__':
     main()
